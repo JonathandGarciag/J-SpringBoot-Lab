ffce5260194073a492a55b5f66743951c418e17e
JonathandGarciag <Jgarcia-2023105@kinal.edu.gt>
2024-07-29 21:10:50 -0600

    Lab#1 aplicación de java utilizando Spring Boot

---
+++ src/main/java/com/jonathangarcia/springwebapcia/SpringwebapciaApplication.java
@@ -0,0 +1,13 @@
+package com.jonathangarcia.springwebapcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebapciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebapciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathangarcia/springwebapcia/SpringwebapciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathangarcia.springwebapcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebapciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
